bd33c885e7b36c0c2500173290cf551baa89b77f
so-ra-no <74533471+so-ra-no@users.noreply.github.com>
2024-01-05 14:42:41 +0800

    Update doctor.py

--- .github/workflows/doctor.py
+++ .github/workflows/doctor.py
@@ -1,60 +1,68 @@
-"""
-Program: doctor.py
-Author: Ken
-Conducts an interactive session of nondirective psychotherapy.
-"""
-
 import random
 
-class Doctor():
-          def __init__(self):
-                    self.hedges = ("Please tell me more.",
-          "Many of my patients tell me the same thing.",
-          "Please coninue.")
+history = []
 
-                    self.qualifiers = ("Why do you say that ",
-              "You seem to think that ",
-              "Can you explain why ")
+ # All doctors share the same qualifiers, replacements, and hedges.    
 
-                    self.replacements = {"I":"you", "me":"you", "my":"your",
-                "we":"you", "us":"you", "mine":"yours"} 
+QUALIFIERS = ['Why do you say that ', 'You seem to think that ',
+                  'Did I just hear you say that ',
+                  'Why do you believe that ']
 
-def greeting(self):
-          return "Good morning, I hope you are well today. What can I do for you?"
+REPLACEMENTS = {'i': 'you', 'me': 'you', 'my': 'your',
+                    'we': 'you', 'us': 'you', 'am': 'are',
+                    'you': 'i', 'you': 'I'}
 
-def signoff(self):
-          return "Thank you for consulting me, have a nice day!"
+HEDGES = ['Go on.', 'I would like to hear more about that.',
+              'And what do you think about this?', 'Please continue.']
+class Doctor(object):
 
-def reply(self, sentence):
-    """Implements two different reply strategies."""
-    probability = random.randint(1, 4)
-    if probability == 1:
-        return random.choice(self.hedges)
-    else:
-        return random.choice(self.qualifiers) + self.changePerson(sentence)
 
-def changePerson(self, sentence):
-    """Replaces first person pronouns with second person
-    pronouns."""
-    words = sentence.split()
-    replyWords = []
-    for word in words:
-        replyWords.append(self.replacements.get(word, word))
-    return " ".join(replyWords) 
+    def greeting():
+        greeting = "Hello, how can I help you today?"
+        return greeting
+
+    def reply(sentence):
+        """Implements three different reply strategies."""
+        probability = random.randint(1, 5)
+        if probability in (1, 2):
+            # Just hedge
+            answer = random.choice(HEDGES)
+        elif probability == 3 and len(history) > 3:
+            # Go back to an earlier topic
+            answer = "Earlier you said that " + \
+                 changePerson(random.choice(history))
+        else:
+            # Transform the current input
+            answer = random.choice(QUALIFIERS) + Doctor.changePerson(sentence)
+        # Always add the current sentence to the history list
+        history.append(sentence)
+        return answer
+
+    def changePerson(sentence):
+        """Replaces first person pronouns with second person
+        pronouns."""
+        words = sentence.split()
+        replyWords = []
+        for word in words:
+           replyWords.append(REPLACEMENTS.get(word, word))
+        return " ".join(replyWords) 
+
+    def farewell(sentence):
+        so_long = "Have a nice day!"
+        return so_long
 
 def main():
-
-    print("Good morning, I hope you are well today.")
-    
-    print("What can I do for you?")
+    """Handles the interaction between patient and doctor."""
+    #print("Good morning, I hope you are well today.")
+    #print("What can I do for you?")
+    print(Doctor.greeting())
     while True:
         sentence = input("\n>> ")
         if sentence.upper() == "QUIT":
-            print("Have a nice day!")
+            print(Doctor.farewell(sentence))
             break
-        print(reply(sentence))
+        print(Doctor.reply(sentence))
 
 # The entry point for program execution
 if __name__ == "__main__":
     main()
-
