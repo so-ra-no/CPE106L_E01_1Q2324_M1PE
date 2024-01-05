6dcd9967a757da4301e60f1be6c6b44dd6a559eb
so-ra-no <74533471+so-ra-no@users.noreply.github.com>
2024-01-05 14:32:35 +0800

    Update doctor.py

--- .github/workflows/doctor.py
+++ .github/workflows/doctor.py
@@ -6,37 +6,46 @@ Conducts an interactive session of nondirective psychotherapy.
 
 import random
 
-hedges = ("Please tell me more.",
+class Doctor():
+          def __init__(self):
+                    self.hedges = ("Please tell me more.",
           "Many of my patients tell me the same thing.",
           "Please coninue.")
 
-qualifiers = ("Why do you say that ",
+                    self.qualifiers = ("Why do you say that ",
               "You seem to think that ",
               "Can you explain why ")
 
-replacements = {"I":"you", "me":"you", "my":"your",
+                    self.replacements = {"I":"you", "me":"you", "my":"your",
                 "we":"you", "us":"you", "mine":"yours"} 
 
-def reply(sentence):
+def greeting(self):
+          return "Good morning, I hope you are well today. What can I do for you?"
+
+def signoff(self):
+          return "Thank you for consulting me, have a nice day!"
+
+def reply(self, sentence):
     """Implements two different reply strategies."""
     probability = random.randint(1, 4)
     if probability == 1:
-        return random.choice(hedges)
+        return random.choice(self.hedges)
     else:
-        return random.choice(qualifiers) + changePerson(sentence)
+        return random.choice(self.qualifiers) + self.changePerson(sentence)
 
-def changePerson(sentence):
+def changePerson(self, sentence):
     """Replaces first person pronouns with second person
     pronouns."""
     words = sentence.split()
     replyWords = []
     for word in words:
-        replyWords.append(replacements.get(word, word))
+        replyWords.append(self.replacements.get(word, word))
     return " ".join(replyWords) 
 
 def main():
-    """Handles the interaction between patient and doctor."""
+
     print("Good morning, I hope you are well today.")
+    
     print("What can I do for you?")
     while True:
         sentence = input("\n>> ")
